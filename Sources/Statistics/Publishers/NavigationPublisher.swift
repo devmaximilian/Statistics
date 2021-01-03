efa358bc7fdee48a50843a51e99e2cf753b9687a
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-01-03 17:55:06 +0100

    Added NavigationPublisher

---
+++ Sources/Statistics/Publishers/NavigationPublisher.swift
@@ -0,0 +1,78 @@
+import Foundation
+import Combine
+
+public struct NavigationPublisher: Publisher {
+    public typealias Output = [NavigationLink]
+    public typealias Failure = Error
+    
+    typealias Upstream = AnyPublisher<StatisticsClient.Response, Error>
+    
+    private let upstream: Upstream
+
+    init(with upstream: Upstream) {
+        self.upstream = upstream
+    }
+    
+    public func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+        let subscription = Inner(subscriber: subscriber)
+        upstream.subscribe(subscription)
+        subscriber.receive(subscription: subscription)
+    }
+}
+
+// MARK: Subscription
+
+extension NavigationPublisher {
+    fileprivate final class Inner<S: Subscriber>: Subscription, Subscriber where S.Input == Output, S.Failure == Failure {
+        typealias Input = Upstream.Output
+        
+        private var upstreamSubscription: Subscription?
+        private var downstreamSubscriber: S?
+        
+        init(subscriber: S) {
+            self.downstreamSubscriber = subscriber
+        }
+        
+        // MARK: Subscription conformance
+        
+        func request(_ demand: Subscribers.Demand) {
+            guard let upstream = self.upstreamSubscription else { return }
+            
+            upstream.request(demand)
+        }
+        
+        func cancel() {
+            self.downstreamSubscriber = nil
+            self.upstreamSubscription = nil
+        }
+        
+        // MARK: Subscriber conformance
+        
+        func receive(subscription: Subscription) {
+            self.upstreamSubscription = subscription
+        }
+        
+        func receive(_ input: Upstream.Output) -> Subscribers.Demand {
+            guard let downstream = self.downstreamSubscriber else { return .none }
+            
+            do {
+                let decoder = JSONDecoder()
+                let output = try decoder.decode(Output.self, from: input.data)
+                return downstream.receive(output)
+            } catch {
+                downstream.receive(completion: .failure(error))
+            }
+            
+            return .none
+        }
+        
+        func receive(completion: Subscribers.Completion<NavigationPublisher.Upstream.Failure>) {
+            self.upstreamSubscription = nil
+            
+            guard let downstream = self.downstreamSubscriber else {
+                return
+            }
+            downstream.receive(completion: completion)
+        }
+    }
+}
