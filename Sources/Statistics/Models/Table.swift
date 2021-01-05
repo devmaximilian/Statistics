8d4a5fafd84357fa8ea0ef6ed057e29d0fabed89
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-01-05 17:12:29 +0100

    Moved table internals to Table

--- Sources/Statistics/Models/Table.swift
+++ Sources/Statistics/Models/Table.swift
@@ -8,13 +8,64 @@
 import Foundation
 
 public struct Table: Decodable {
-    let columns: [Column]
-    let data: [Row]
+    let columns: [TableColumn]
+    let data: [TableRow]
 //    let comments: [String]
-    let metadata: [Details]
+    let metadata: [TableDetails]
 }
 
 // TODO: Add specific types of tables, such as single key-table
+
+// MARK: Column
+
+struct TableColumn: Decodable {
+    let code: String
+    let text: String
+    let type: DataType
+    let comment: String?
+}
+
+enum DataType: String, Decodable {
+    case time = "t"
+    case content = "c"
+    case data = "d"
+    case invalid = ""
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+        
+        self = DataType(rawValue: string) ?? .invalid
+    }
+}
+
+// MARK: Row
+
+struct TableRow: Decodable {
+    let key: [String]
+    let values: [String]
+}
+
+// MARK: Details
+
+struct TableDetails: Decodable {
+    let infofile: String
+    let updated: String
+    let label: String
+    let source: String
+}
+
+// MARK: Convenience
+
+extension String {
+    var double: Double {
+        return Double(self) ?? 0
+    }
+    
+    var int: Int {
+        return Int(self) ?? 0
+    }
+}
 
 extension Table {
     public var comments: [(code: String, text: String, comment: String)] {
@@ -24,9 +75,5 @@ extension Table {
         .map {
             ($0.code, $0.text, $0.comment ?? "")
         }
-    }
-    
-    public var singleKey: Bool {
-        return data.first?.key.count ?? 1 < 2
     }
 }
