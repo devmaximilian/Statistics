50390bf6796be36f11bab0816649cd3537607fe3
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2021-01-06 12:31:49 +0100

    Removed Client+Error

--- Sources/Statistics/Client/Client+Error.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public enum StatisticsClientError: Error {
-    case networkError(URLError)
-    case erased(Error)
-}
--- Sources/Statistics/Extensions/URLSession+DataTaskPublisher+Response.swift
+++ Sources/Statistics/Extensions/URLSession+DataTaskPublisher+Response.swift
@@ -5,15 +5,11 @@ extension URLSession.DataTaskPublisher {
     func validateResponse() -> AnyPublisher<(data: Data, response: URLResponse), Error> {
         self.tryMap { (data: Data, response: URLResponse) -> (data: Data, response: URLResponse) in
             guard let response = response as? HTTPURLResponse else {
-                throw StatisticsClientError.networkError(
-                    URLError(.unknown)
-                )
+                throw URLError(.unknown)
             }
             guard 200...299 ~= response.statusCode else {
-                throw StatisticsClientError.networkError(
-                    URLError(
-                        URLError.Code(rawValue: response.statusCode)
-                    )
+                throw URLError(
+                    URLError.Code(rawValue: response.statusCode)
                 )
             }
             return (data, response)
