c6876ff46ef00430b1fa3742a15c4a7db8a9cef3
Maximilian Wendel <devmaximilian@users.noreply.github.com>
2021-01-06 08:37:04 +0100

    Update Client.swift

--- Sources/Statistics/Client/Client.swift
+++ Sources/Statistics/Client/Client.swift
@@ -16,8 +16,8 @@ public final class StatisticsClient {
     /// Returns a publisher that wraps a navigation structure request for a given navigation link.
     ///
     /// The publisher publishes data when the request completes, or terminates if the task fails.
-    /// - Parameter link: The NavigationLink for which to create a request.
-    /// - Returns: A publisher that wraps a navigation structure request for the NavigationLink.
+    /// - Parameter link: The navigation link for which to create a request.
+    /// - Returns: A publisher that wraps a navigation structure request for the navigation link.
     public func navigationPublisher(for link: NavigationLink) -> NavigationPublisher {
         return NavigationPublisher(
             client: self,
@@ -37,31 +37,55 @@ public final class StatisticsClient {
         )
     }
     
-    public func tableDescriptorPublisher(for link: NavigationLink, method: NavigationLink) -> TableDescriptorPublisher {
+    /// Returns a publisher that wraps a table descriptor request for a given subject.
+    ///
+    /// The publisher publishes data when the request completes, or terminates if the task fails.
+    /// - Parameter link: The navigation link indicating the subject area of the subject.
+    /// - Parameter subject: The navigation link specifying the subject for which to create a request.
+    /// - Returns: A publisher that wraps a table descriptor request for the given subject.
+    public func tableDescriptorPublisher(for link: NavigationLink, subject: NavigationLink) -> TableDescriptorPublisher {
         return TableDescriptorPublisher(
             client: self,
-            request: get("\(link.id)/\(method.id)")
+            request: get("\(link.id)/\(subject.id)")
         )
     }
     
-    public func tableDescriptorPublisher(for code: String, method: String) -> TablePublisher {
+    /// Returns a publisher that wraps a table descriptor request for a given subject.
+    ///
+    /// The publisher publishes data when the request completes, or terminates if the task fails.
+    /// - Parameter link: The subject area of the subject.
+    /// - Parameter subject: The subject for which to create a request.
+    /// - Returns: A publisher that wraps a table descriptor request for the given subject.
+    public func tableDescriptorPublisher(for code: String, subject: String) -> TablePublisher {
         return TablePublisher(
             client: self,
-            request: get("\(code)/\(method)")
+            request: get("\(code)/\(subject)")
         )
     }
     
-    public func tablePublisher(for link: NavigationLink, method: NavigationLink) -> TablePublisher {
+    /// Returns a publisher that wraps a table descriptor request for a given subject.
+    ///
+    /// The publisher publishes data when the request completes, or terminates if the task fails.
+    /// - Parameter link: The navigation link indicating the subject area of the subject.
+    /// - Parameter subject: The navigation link specifying the subject for which to create a request.
+    /// - Returns: A publisher that wraps a table descriptor request for the given subject.
+    public func tablePublisher(for link: NavigationLink, subject: NavigationLink) -> TablePublisher {
         return TablePublisher(
             client: self,
-            request: post("\(link.id)/\(method.id)")
+            request: post("\(link.id)/\(subject.id)")
         )
     }
     
-    public func tablePublisher(for code: String, method: String) -> TablePublisher {
+    /// Returns a publisher that wraps a table request for a given subject.
+    ///
+    /// The publisher publishes data when the request completes, or terminates if the task fails.
+    /// - Parameter link: The subject area of the subject.
+    /// - Parameter subject: The subject for which to create a request.
+    /// - Returns: A publisher that wraps a table request for the given subject.
+    public func tablePublisher(for code: String, subject: String) -> TablePublisher {
         return TablePublisher(
             client: self,
-            request: post("\(code)/\(method)")
+            request: post("\(code)/\(subject)")
         )
     }
 }
